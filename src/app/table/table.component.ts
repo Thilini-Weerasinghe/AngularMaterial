571a95abb0a9a7b8d5e80b5b5074a013d263d1d3
Thilini-Weerasinghe <thiliniweerasinghe09@gmail.com>
2018-11-05 23:43:44 +0530

    finalize the smart table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -17,9 +17,16 @@ export class TableComponent implements OnInit {
       .getCharacters()
       .subscribe((data: Table[]) => {
         this.characters = data;
+         console.log(this.characters);
     }); 
+   console.log(this.characters);
   }
 
+  savedata(){
+    console.log("sbbjsbcbksnclsnc");
+  }
+
+//  console.log(characters);
   settings = {
     columns: {
       id: {
@@ -33,5 +40,7 @@ export class TableComponent implements OnInit {
       }
     }
   };
+
+  
 
 }
